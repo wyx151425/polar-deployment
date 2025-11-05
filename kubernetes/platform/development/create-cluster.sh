5c0610f1d5a527d3bd9b0e0ed4350baa43b14d67
wyx151425 <wyx151425@163.com>
2025-11-05 21:02:57 +0800

    feat: 修改部署支撑服务的脚本

--- kubernetes/platform/development/create-cluster.sh
+++ kubernetes/platform/development/create-cluster.sh
@@ -10,9 +10,33 @@ minikube addons enable ingress --profile polar
 
 sleep 15
 
-echo "\n📦 Deploying platform services..."
+echo "\n📦 Deploying Keycloak..."
 
-kubectl apply -f services
+kubectl apply -f services/keycloak-config.yml
+kubectl apply -f services/keycloak.yml
+
+sleep 5
+
+echo "\n⌛ Waiting for Keycloak to be deployed..."
+
+while [ $(kubectl get pod -l app=polar-keycloak | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for Keycloak to be ready..."
+
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=app=polar-keycloak \
+  --timeout=300s
+
+echo "\n⌛ Ensuring Keycloak Ingress is created..."
+
+kubectl apply -f services/keycloak.yml
+
+echo "\n📦 Deploying PostgreSQL..."
+
+kubectl apply -f services/postgresql.yml
 
 sleep 5
 
@@ -29,6 +53,12 @@ kubectl wait \
   --selector=db=polar-postgres \
   --timeout=180s
 
+echo "\n📦 Deploying Redis..."
+
+kubectl apply -f services/redis.yml
+
+sleep 5
+
 echo "\n⌛ Waiting for Redis to be deployed..."
 
 while [ $(kubectl get pod -l db=polar-redis | wc -l) -eq 0 ] ; do
@@ -40,6 +70,44 @@ echo "\n⌛ Waiting for Redis to be ready..."
 kubectl wait \
   --for=condition=ready pod \
   --selector=db=polar-redis \
+  --timeout=180s
+
+echo "\n📦 Deploying RabbitMQ..."
+
+kubectl apply -f services/rabbitmq.yml
+
+sleep 5
+
+echo "\n⌛ Waiting for RabbitMQ to be deployed..."
+
+while [ $(kubectl get pod -l db=polar-rabbitmq | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for RabbitMQ to be ready..."
+
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=db=polar-rabbitmq \
+  --timeout=180s
+
+echo "\n📦 Deploying Polar UI..."
+
+kubectl apply -f services/polar-ui.yml
+
+sleep 5
+
+echo "\n⌛ Waiting for Polar UI to be deployed..."
+
+while [ $(kubectl get pod -l app=polar-ui | wc -l) -eq 0 ] ; do
+  sleep 5
+done
+
+echo "\n⌛ Waiting for Polar UI to be ready..."
+
+kubectl wait \
+  --for=condition=ready pod \
+  --selector=app=polar-ui \
   --timeout=180s
 
 echo "\n⛵ Happy Sailing!\n"
